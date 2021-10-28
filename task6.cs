67c1e985ef5a420d32c4d92f85cc99947bc5e154
Hajili <tu201806363@aztu.local>
2021-10-28 07:44:08 +0400

    exercises method

---
+++ task6.cs
@@ -0,0 +1,15 @@
+
+class Program
+{
+    static void Main() {
+        
+        int reqem12 = int.Parse(Console.ReadLine());
+        cube(reqem12);
+        
+    }
+    
+    static void cube(int reqem){
+        Console.WriteLine(reqem*reqem*reqem);
+    }
+  
+}
